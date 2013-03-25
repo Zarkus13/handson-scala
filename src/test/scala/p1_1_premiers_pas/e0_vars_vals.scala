bc6ca793b50e6f8f42a7d13d7f7073232abff591
Mathieu Chataigner <mathieu.chataigner@gmail.com>
2013-03-25 18:26:16 +0100

    remove solution from test 0

--- src/test/scala/p1_1_premiers_pas/e0_vars_vals.scala
+++ src/test/scala/p1_1_premiers_pas/e0_vars_vals.scala
@@ -17,10 +17,10 @@ class e0_vars_vals extends HandsOnSuiteP1 {
 
   exercice("Les vars peuvent être réaffectées") {
     var a = 5
-    a should be(5)
+    a should be(__)
 
     a = 7
-    a should be(7)
+    a should be(__)
   }
 
   exercice("Par contre les vals sont immuables (équivalent de final Java), elles ne peuvent pas être réaffectées") {
