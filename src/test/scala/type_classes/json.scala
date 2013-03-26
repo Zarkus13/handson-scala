485c57824ec3fe93120263e4a17e13a929308975
Mathieu Chataigner <mathieu.chataigner@gmail.com>
2013-03-26 16:38:17 +0100

    ajout de commentaires pour les typeclasse

--- src/test/scala/type_classes/json.scala
+++ src/test/scala/type_classes/json.scala
@@ -1,10 +1,16 @@
 package type_classes
 
+/*
+* ici on défini notre arborescence de type json,
+* avec string, number, seq et object.
+* représentant tous les types json possibles
+* et étendant le même trait JsValue
+*/
 sealed trait JsValue
+
 case class JsString(s:String) extends JsValue{
   override def toString():String= ???
 }
-
 case class JsNumber(n:Number) extends JsValue{
   override def toString():String= ???
 }
@@ -14,13 +20,29 @@ case class JsSeq(seq:Seq[JsValue]) extends JsValue{
 case class JsObject(properties:Map[String, JsValue]) extends JsValue{
   override def toString():String= ???
 }
+
+/*
+* On crée un objet compagnon permettant de créer un JsObject
+* à partir d'une liste de clé valeur de type String -> JsValue
+*/
 object JsObject{
   def apply(properties:(String,JsValue)*):JsObject= ???
 }
 
+/*
+* Voici une type class
+* C'est un trait générique qui permet de prendre un type A et de retourner
+* la JsValue le représentant.
+*/
 trait Writer[A]{
   def write(value:A):JsValue
 }
+
+/*
+* l'objet compagnon l'accompagnant sert à créer des writer sans à avoir à spécifier
+* le type générique, car il est inféré à la compilation grâce au type de la fonction
+* passée en paramètre à apply
+*/
 object Writer{
   def apply[A]( f: A=>JsValue ):Writer[A]={
     new Writer[A]{
@@ -29,6 +51,11 @@ object Writer{
   }
 }
 
+/*
+* Dans cet objet on défini les writer pour les types qui nous intéresse.
+* L'intérêt de les marquer implicit est que si on les importe à un endroit du code
+* ils seront présent pour tout de scope du code.
+*/
 object Implicits {
   implicit val stringWriter=Writer { s:String=> ??? }
   implicit val intWriter=Writer { n:Int=> ??? }
@@ -37,6 +64,12 @@ object Implicits {
   implicit def seqWriter[B](implicit writer:Writer[B])= Writer { seq:Seq[B] => ??? }
 }
 
+/*
+* Enfin le sérialiseur qui prend un objet de type A et implicitement
+* un writer de type Writer[A]. Il n'a donc pas besoin d'importer ici
+* les writers en question, il suffira de les avoir dans le scope implicit
+* du cope appelant.
+*/
 object Json{
   def toJson[A](value:A)(implicit writer:Writer[A]):JsValue={
     writer.write(value)
